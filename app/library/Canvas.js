d8a0b48c74c6bc95858726d44242ceccff07ee77
lommi <jonorn@gmail.com>
2016-07-15 16:24:15 +0000

    Scale example

--- app/library/Canvas.js
+++ app/library/Canvas.js
@@ -20,11 +20,28 @@ class Canvas
     fill() { /* Maybe not needed later */
         this._ctx.fillStyle = "#000000";
 		this._ctx.fillRect(0, 0, this._object.width, this._object.height);
+        this._ctx.beginPath();
+        this._ctx.strokeStyle = "#fff";
+        this._ctx.lineWidth = 0.8;
+        this._ctx.strokeRect(0,0,16,16);
+
+
+
+        this._ctx.closePath();
+        this._ctx.fill();
     }
     
     resize() {    		
         this._object.style.width = this.widthString();
     	this._object.style.height = this.heightString();    		
+        // If we wish to scale the game, on bigger or smaller screens
+        // we could set the canvas size to be different
+        this._ctx.canvas.width = this.width();
+        this._ctx.canvas.height = this.width();        
+        // For example
+        this._ctx.canvas.width = 640;
+        this._ctx.canvas.height = 320;        
+        
 		this.fill();
     	
        	// !!DEBUG
@@ -33,10 +50,17 @@ class Canvas
     }
     
     heightString() {
-        return window.innerHeight+"px";
+        return this.height()+"px";
+    }
+    height() {
+        return window.innerHeight;
     }
     
+    
     widthString() {
-        return window.innerWidth+"px";
+        return this.width()+"px";
+    }
+    width() {
+        return window.innerWidth;
     }
 }
--- app/library/init.js
+++ app/library/init.js
@@ -1,3 +1,5 @@
 'use strict';	
 var canvas = new Canvas();
 var menu = new Menu();
+
+

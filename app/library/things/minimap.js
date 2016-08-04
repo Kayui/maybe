5b8649e3b138d5b1e6797dc57956bc7998fe369b
lommi <jonorn@gmail.com>
2016-08-04 08:34:33 +0000

    Minimap fix

--- app/library/things/minimap.js
+++ app/library/things/minimap.js
@@ -7,11 +7,29 @@ class Minimap {
         this.width = ($_.map.width * $_.map.tile.x) / 10;
         this.height = ($_.map.height * $_.map.tile.y) / 10;
         this.drawEv = this.draw.bind(this);
+
+        // Creating elements
+        this.minimap = document.createElement('div');
+        this.viewport = document.createElement('div');
+        this.minimap.className = "minimap";
+        this.viewport.className = "viewport";
+        document.body.appendChild(this.minimap);
+        this.minimap.appendChild(this.viewport);
+        this.minimap.addEventListener('click', this.click);
         $_.getEvent('SpritesReady', this.drawEv);
         // this.draw();
         // x and y represent the top left corner of the map
         // aim for bottom right corner of the screen
         // draw as spritegroup
+    }
+
+    click(ev) {
+      var x = ev.layerX;
+      var y = ev.layerY;
+      var cameraPosY = (y / 200) * ($_.map.height * $_.map.tile.y);
+
+      $_.game.camera.y = (y / 200) * ($_.map.height * $_.map.tile.y) - ($_.game.camera.height / 2);
+      $_.game.camera.x = (x / 200) * ($_.map.width * $_.map.tile.x) - ($_.game.camera.width / 2);
     }
 
     draw() {
@@ -29,9 +47,9 @@ class Minimap {
         }
         var mapTexture = $_.game.create.texture('someKey', mapData, 8, 8, 0);
         this.url = mapTexture.baseTexture.source.currentSrc;
-        document.getElementById('minimap').style.backgroundImage = "url(" + this.url + ")";
-        document.getElementById('minimap').style.backgroundSize = "contain";
-        this.viewport = document.getElementById('minimapviewport');
+        this.minimap.style.backgroundImage = "url(" + this.url + ")";
+        this.minimap.style.backgroundSize = "contain";
+
 
         /* $_.watchObject("minimap.mapTexture.baseTexture.hasLoaded",function(){
          $_.minimap.minimap = $_.game.add.sprite(0,0, 'someKey');
@@ -84,13 +102,13 @@ class Minimap {
 
         var originalCameraPosX = $_.game.camera.x;
         var originalCameraPosY = $_.game.camera.y;
-        var x = originalCameraPosX === 0 ? 0 : originalCameraPosX  / originalMapWidth; 
+        var x = originalCameraPosX === 0 ? 0 : originalCameraPosX  / originalMapWidth;
         var y = originalCameraPosY === 0 ? 0 : originalCameraPosY  / originalMapHeight;
         this.viewport.style.left = mapWidth * x + "px";
         this.viewport.style.top  = mapHeight * y + "px";
 
 
-        //this.viewPort.style.width = 
+        //this.viewPort.style.width =
         // Get there the camera is and update the camera rectangle
     }
 
